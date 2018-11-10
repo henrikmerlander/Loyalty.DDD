77c8fd24c4c0efe382538e77374e53fc03be33d9
Henrik Merlander <henrik.merlander@ezy.se>
2018-11-10 15:44:33 +0100

    Implement create wallet command and command handler

---
+++ src/Application.Tests/CreateWalletCommandHandlerTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Commands;
+using Domain.WalletAggregate;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+
+namespace Application.Tests
+{
+    internal class CreateWalletCommandHandlerTest
+    {
+        private readonly Mock<IWalletRepository> _walletRepositoryMock;
+        private readonly Mock<IMediator> _mediator;
+
+        public CreateWalletCommandHandlerTest()
+        {
+            _walletRepositoryMock = new Mock<IWalletRepository>();
+            _mediator = new Mock<IMediator>();
+        }
+
+        [Test]
+        public async Task Handle_returns_false_if_wallet_is_not_persisted()
+        {
+            var createWalletCommand = new CreateWalletCommand();
+
+            _walletRepositoryMock.Setup(walletRepo => walletRepo.UnitOfWork.SaveEntitiesAsync(default(CancellationToken)))
+                .Returns(Task.FromResult(false));
+
+            var handler = new CreateWalletCommandHandler(_mediator.Object, _walletRepositoryMock.Object);
+            var cancellationToken = new CancellationToken();
+            var result = await handler.Handle(createWalletCommand, cancellationToken);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public async Task Handle_returns_true_if_wallet_is_persisted()
+        {
+            var createWalletCommand = new CreateWalletCommand();
+
+            _walletRepositoryMock.Setup(walletRepo => walletRepo.UnitOfWork.SaveEntitiesAsync(default(CancellationToken)))
+                .Returns(Task.FromResult(true));
+
+            var handler = new CreateWalletCommandHandler(_mediator.Object, _walletRepositoryMock.Object);
+            var cancellationToken = new CancellationToken();
+            var result = await handler.Handle(createWalletCommand, cancellationToken);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ src/Application/Commands/CreateWalletCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.WalletAggregate;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateWalletCommandHandler : IRequestHandler<CreateWalletCommand, bool>
+    {
+        private readonly IWalletRepository _walletRepository;
+        private readonly IMediator _mediator;
+
+        public CreateWalletCommandHandler(IMediator mediator, IWalletRepository walletRepository)
+        {
+            _walletRepository = walletRepository ?? throw new ArgumentNullException(nameof(walletRepository));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<bool> Handle(CreateWalletCommand request, CancellationToken cancellationToken)
+        {
+            var wallet = new Wallet();
+
+            _walletRepository.Add(wallet);
+
+            return await _walletRepository.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
