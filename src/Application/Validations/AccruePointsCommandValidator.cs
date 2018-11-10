5e5b0c5be0806fa7f992a9bd334bd7869f788aab
Henrik Merlander <henrik.merlander@ezy.se>
2018-11-10 20:55:16 +0100

    Add command validation rules

---
+++ src/Application/Validations/AccruePointsCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Commands;
+using FluentValidation;
+
+namespace Application.Validations
+{
+    public class AccruePointsCommandValidator : AbstractValidator<AccruePointsCommand>
+    {
+        public AccruePointsCommandValidator()
+        {
+            RuleFor(command => command.Points).GreaterThan(0);
+        }
+    }
+}
---
+++ src/Application/Validations/RedeemPointsCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Commands;
+using FluentValidation;
+
+namespace Application.Validations
+{
+    public class RedeemPointsCommandValidator : AbstractValidator<RedeemPointsCommand>
+    {
+        public RedeemPointsCommandValidator()
+        {
+            RuleFor(command => command.Points).GreaterThan(0);
+        }
+    }
+}
