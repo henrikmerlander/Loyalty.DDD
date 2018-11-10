416516b7207c11f2c7fcc63228aaf25ae7e111ab
Henrik Merlander <henrik.merlander@ezy.se>
2018-11-10 18:49:53 +0100

    Add wallet controller with create wallet functionality

---
+++ src/API/Controllers/WalletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class WalletController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public WalletController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateWalletCommand command)
+        {
+            var commandResult = await _mediator.Send(command);
+
+            return commandResult ? Ok() : (IActionResult)BadRequest();
+        }
+    }
+}
